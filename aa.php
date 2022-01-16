4faa4a88c3af7de361f72f67a6bf51dfe97730e2
Al-khalid <92049116+Al-khalid@users.noreply.github.com>
2022-01-16 03:26:34 +0300

    Update aa.php

--- aa.php
+++ aa.php
@@ -1,9 +1,8 @@
 <?php
 $read = explode("\n", file_get_contents("x.txt"));
 $y = [" ",".","-"];
-$x   = ["1","0"," "]; 
+$x = ["1","0"," "]; 
 $str = str_replace($y, $x, $read);
-
 foreach($str as $x)
 echo $x;
 ?>
